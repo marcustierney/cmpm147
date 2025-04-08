5e595ee075216acbd7b01e8a5142e3a704980ea7
marcustierney <131930897+marcustierney@users.noreply.github.com>
2025-04-07 23:22:03 -0700

    Update sketch.js

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -1,6 +1,6 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Sketch file
+// Author: Marcus Tierney
+// Date: 4/7/25
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
